f4dd80680667a6b4869b3ef448f499d2f4501baa
Sunanda Wasala <sunanda@listudiosl.com>
2022-09-05 21:11:20 +0530

    login and register page style chnages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,92 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+@extends('layouts.app') 
+@section('content')
 
-        @if (session('status'))
+<style>
+    ul.mt-3.list-disc.list-inside.text-sm.text-red-600 {
+    color: red !important;
+    margin-left:-25px
+    }
+    .font-medium.text-red-600 {
+    display: none;
+    }
+</style>
+
+
+
+<div class="register-box">
+
+
+  @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
             </div>
-        @endif
+  @endif
 
-        <form method="POST" action="{{ route('login') }}">
+ 
+
+  <div class="card">
+    <div class="card-body register-card-body">
+      <p class="login-box-msg">Login</p>
+      <x-jet-validation-errors class="md-10" />
+
+      <form method="POST" action="{{ route('login') }}">
             @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+       
+        <div class="input-group mb-3">
+            <label for="email" value="{{ __('Email') }}"></label>
+          <input id="email" type="email" class="form-control" placeholder="Email" name="email" :value="old('email')">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <label for="password" value="{{ __('Password') }}"></label>
+          <input type="password" id="password" class="form-control" placeholder="Password" name="password" autocomplete="new-password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
             </div>
+          </div>
+        </div>
+       
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+        <div class="row">
+        <div class="col-6">
+           <label for="remember_me"></label>
+           <input type="checkbox" id="remember_me" name="remember">
+           <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+          </div>
+         
+          <div class="col-6">
+            <button type="submit" class="btn btn-primary btn-block"> {{ __('Log in') }}</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+      
+      @if (Route::has('password.request'))
+      <a href="{{ route('password.request') }}" class="text-center">{{ __('Forgot your password?') }}</a>
+      @endif
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+
+
+
+@endsection
+
+       
+
+
+
+
+
+   
+
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,29 @@
 @extends('layouts.app') 
 @section('content')
 
+<style>
+    ul.mt-3.list-disc.list-inside.text-sm.text-red-600 {
+    color: red !important;
+    margin-left:-25px
+    }
+    .font-medium.text-red-600 {
+    display: none;
+    }
+</style>
+
 <div class="register-box">
  
 
   <div class="card">
     <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">Register</p>
+      <x-jet-validation-errors class="mb-4" />
 
       <form method="POST" action="{{ route('register') }}">
             @csrf
         <div class="input-group mb-3">
             <label for="name" value="{{ __('Name') }}"></label>
-          <input id="name" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" class="form-control" placeholder="name">
+          <input id="name" type="text" name="name" :value="old('name')"  autofocus autocomplete="name" class="form-control" placeholder="name">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -21,7 +32,7 @@
         </div>
         <div class="input-group mb-3">
             <label for="email" value="{{ __('Email') }}"></label>
-          <input id="email" type="email" class="form-control" placeholder="Email" name="email" :value="old('email')" required>
+          <input id="email" type="email" class="form-control" placeholder="Email" name="email" :value="old('email')" >
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -30,7 +41,7 @@
         </div>
         <div class="input-group mb-3">
           <label for="password" value="{{ __('Password') }}"></label>
-          <input type="password" id="password" class="form-control" placeholder="Password" name="password" required autocomplete="new-password">
+          <input type="password" id="password" class="form-control" placeholder="Password" name="password"  autocomplete="new-password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -39,7 +50,7 @@
         </div>
         <div class="input-group mb-3">
           <label for="password_confirmation" value="{{ __('Confirm Password') }}"></label>
-          <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" required autocomplete="new-password">
+          <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" placeholder="Confirm Password"  autocomplete="new-password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -56,9 +67,13 @@
         </div>
       </form>
 
-      
+      <div class="row">
+         
+          <div class="col-12 text-center">
 
       <a href="{{ route('login') }}" class="text-center">{{ __('Already registered?') }}</a>
+  </div>
+  </div>
     </div>
     <!-- /.form-box -->
   </div><!-- /.card -->
